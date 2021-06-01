d50332f81fb53deda2b2ea45c4461db806579a25
majinlong <qq111222>
2021-06-01 15:30:05 +0800

    0.1.1 - 添加defaultRedisTemplate注解

--- src/main/java/com/jalen/azeroth/cache/annotation/DefaultRedisTemplate.java
+++ src/main/java/com/jalen/azeroth/cache/annotation/DefaultRedisTemplate.java
@@ -3,7 +3,11 @@ package com.jalen.azeroth.cache.annotation;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import javax.annotation.Resource;
+import java.lang.annotation.*;
 
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
 @Resource(name = "defaultRedisTemplate", type = RedisTemplate.class)
 public @interface DefaultRedisTemplate {
 }
